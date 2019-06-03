142eb4f6415dd0af7924e9c13c4f85112971acb7
xiaojk007 <402502815@qq.com>
2019-06-03 21:44:47 +0800

    新增项目222

--- src/main/java/Demos.java
+++ src/main/java/Demos.java
@@ -1,6 +1,6 @@
 public class Demos {
     public static void main(String[] args){
              System.out.println("this is branch155fsdf55");
-             System.out.println();
+             System.out.println(77777);
     }
 }
